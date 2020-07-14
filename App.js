8c6c88ad25695d52b33a26365d495b6a23e63265
CoverKey <cskinner@coverkey.com>
2020-07-14 08:37:10 -0700

    Initial commit of bare template

--- App.js
+++ App.js
@@ -4,16 +4,26 @@ import { StyleSheet, Text, View } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'dodgerblue',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 40,
+    fontWeight: 'bold',
+  },
+  text2: {
+    fontSize: 27,
+    fontWeight: '100',
+    alignItems: 'center',
   },
 });
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>MyApp Template</Text>
+      <Text style={styles.text2}>I am really excited about this!</Text>
     </View>
   );
 }
